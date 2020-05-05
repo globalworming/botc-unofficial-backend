653c78b3012abe47bfd16c82e4384a03c5d62cb1
wormi <andreas.worm@headissue.com>
2020-05-05 14:00:04 +0200

    players can join via rest

--- src/main/kotlin/com/headissue/botcunofficialbackend/controller/GameTableController.kt
+++ src/main/kotlin/com/headissue/botcunofficialbackend/controller/GameTableController.kt
@@ -1,24 +1,43 @@
 package com.headissue.botcunofficialbackend.controller
 
 import com.headissue.botcunofficialbackend.model.GameTable
+import com.headissue.botcunofficialbackend.model.Player
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.*
 
-import org.springframework.web.bind.annotation.RestController
 import java.util.*
+import kotlin.NoSuchElementException
 
 
 @RestController
 class GameTableController {
 
-  val gameTables = mutableMapOf<UUID, GameTable>()
+  val gameTables = mutableMapOf<String, GameTable>()
+
+  @GetMapping("/gametables")
+  fun askForGameTables(): ResponseEntity<Collection<GameTable>> {
+    return ResponseEntity.ok(gameTables.values)
+  }
 
   @PostMapping("/gametables")
-  fun createNewGameTable(@RequestParam name: String): ResponseEntity<Any> {
-    val uuid = UUID.randomUUID()
-    val gameTable = GameTable(uuid, name)
-    gameTables[uuid] = gameTable
+  fun createNewGameTable(@RequestParam id: String): ResponseEntity<GameTable> {
+    val gameTable = GameTable(id)
+    gameTables[id] = gameTable
     return ResponseEntity.ok(gameTable)
   }
+
+  @GetMapping("/gametable/{id}")
+  fun askForGameTable(@PathVariable id: String): ResponseEntity<GameTable> {
+    return ResponseEntity.ok(internalGetTable(id))
+  }
+
+  @PostMapping("/gametable/{id}/players")
+  fun joinGame(@PathVariable id: String, @RequestParam name: String): ResponseEntity<GameTable> {
+    val gameTable = internalGetTable(id)
+    gameTable.players.add(Player(name))
+    return ResponseEntity.ok(gameTable)
+  }
+
+  private fun internalGetTable(id: String) = Optional.ofNullable(gameTables[id]).orElseThrow { NoSuchElementException() }
+
 }
--- src/main/kotlin/com/headissue/botcunofficialbackend/model/GameTable.kt
+++ src/main/kotlin/com/headissue/botcunofficialbackend/model/GameTable.kt
@@ -3,7 +3,6 @@ package com.headissue.botcunofficialbackend.model
 import java.util.*
 
 data class GameTable(
-    val id: UUID,
-    val name: String,
-    val players: List<Player> = listOf()
+    val id: String,
+    val players: MutableList<Player> = mutableListOf()
 )
